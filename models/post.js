daebbfcb2942bd6aaed077543757dea0e8a01d89
OctoPandas <63722857+OctoPandas@users.noreply.github.com>
2021-03-20 03:36:58 +0800

    completed

--- models/post.js
+++ models/post.js
@@ -1,3 +1,6 @@
+const { ObjectId } = require('mongodb')
+
+// 为了简化调用这里直接持有一个 `Promise` 对象
 const postCollection = require('../config/mongodb').getCol('posts')
 
 exports.save = async post => {
@@ -8,5 +11,55 @@ exports.save = async post => {
     return result.ops?.[0]
   } catch (err) {
     throw { msg: 'failed to add a post', err }
+  }
+}
+
+exports.findAll = async () => {
+  try {
+    const collection = await postCollection
+    // `find()` 方法返回一个游标，可以在此基础上进行排序过滤等操作
+    // 这里不必进行 `await` 因为上层一定会 `await findAll()`
+    return collection.find({}).toArray()
+  } catch (err) {
+    throw { msg: 'failed to fetch all posts', err }
+  }
+}
+
+exports.update = async (id, post) => {
+  try {
+    const collection = await postCollection
+    // `findOneAndUpdate` 会返回更新后的问题，而 `updateOne` 则不会
+    const result = await collection.findOneAndUpdate(
+      { _id: ObjectId(id) },
+      { $set: post },
+      { returnOriginal: false }
+    )
+    return result.value
+  } catch (err) {
+    throw { msg: 'failed to update post', err }
+  }
+}
+
+exports.delete = async id => {
+  try {
+    const collection = await postCollection
+    await collection.deleteOne({ _id: ObjectId(id) })
+    // 不需要返回值，没有异常则认为删除成功
+  } catch (err) {
+    throw { msg: 'failed to delete post', err }
+  }
+}
+
+exports.deleteCommnetByUser = async (id, user) => {
+  // { _id, comments: [{ user: '??', comment: '??' }, {}, {}] }
+  // 为了删除里面的嵌套文档
+  try {
+    const collection = await postCollection
+    await collection.updateOne(
+      { _id: ObjectId(id) },
+      { $pull: { comments: { user } } }
+    )
+  } catch (err) {
+    throw { msg: 'failed to delete comments of user', err }
   }
 }
--- routes/post.js
+++ routes/post.js
@@ -4,8 +4,16 @@ const router = express.Router()
 
 const postModel = require('../models/post')
 
-router.get('/', (req, res) => {
-  res.send({ id: nanoid(), title: new Date().toString() })
+router.get('/', async (req, res) => {
+  try {
+    const posts = await postModel.findAll()
+    res.json(posts)
+  } catch (err) {
+    console.error(err)
+    // 这里其实不应该使用 404 状态码的
+    // 如果是为了缓解客户情绪勉强说得过去吧
+    res.status(404).send()
+  }
 })
 
 router.post('/', async (req, res) => {
@@ -18,14 +26,35 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.patch('/:id', (req, res) => {
-  console.log(req.params.id, req.body)
-  res.send()
+router.patch('/:id', async (req, res) => {
+  try {
+    const post = await postModel.update(req.params.id, req.body)
+    res.json(post)
+  } catch (err) {
+    console.error(err)
+    res.status(500).send()
+  }
 })
 
-router.delete('/:id', (req, res) => {
-  console.log(req.params.id, req.body)
-  res.status(204).send()
+router.delete('/:id', async (req, res) => {
+  try {
+    await postModel.delete(req.params.id)
+    res.status(204).send()
+  } catch (err) {
+    console.error(err)
+    res.status(500).send()
+  }
+})
+
+// 删除某 post 下特定 user 的评论
+router.delete('/:id/comment', async (req, res) => {
+  try {
+    await postModel.deleteCommnetByUser(req.params.id, req.body.user)
+    res.status(204).send()
+  } catch (err) {
+    console.error(err)
+    res.status(500).send()
+  }
 })
 
 module.exports = router
